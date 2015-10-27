5cc96cba247a6980a3b86b8eb9f8490a06e6e9af
Andrey Red <mattue@mail.ru>
2015-10-27 20:41:52 +0300

    fixed some errors in tests; added actions test

--- tests/parser_test.js
+++ tests/parser_test.js
@@ -1,22 +1,26 @@
 var Parser = require('../lib/Parser');
+var Actions = require('../lib/Actions');
+var Transformations = require('../lib/Transformations');
 var PhantomEnvironment = require('../lib/PhantomEnvironment');
 var chai = require('chai');
 var expect = chai.expect;
 var path = require('path');
-var _ = require('lodash');
 
+var env = null;
 var uri = 'file://' + path.join(__dirname, '/parser.html');
 
-var env = new PhantomEnvironment({
-    url: uri,
-    screen: {
-        width: 1080,
-        height: 200
-    }
+before(function () {
+    env = new PhantomEnvironment({
+        url: uri,
+        screen: {
+            width: 1080,
+            height: 200
+        }
+    });
 });
 
 describe('Parser', function () {
-    this.timeout(30000);
+    this.timeout(5000);
     describe('#parse', function () {
         it('parse single page', function () {
             var parser = new Parser({
@@ -25,16 +29,16 @@ describe('Parser', function () {
             return parser.parse(
                 {
                     rules: {
-                        scope: '.scrollable > .content > div.get-scope-test-6-passed',
+                        scope: '.scrollable > .content > div.scope-test-6-passed',
                         collection: [[
-                            {name: 'column1', scope: 'div.get-scope-test-6-passed-column1'},
+                            {name: 'column1', scope: 'div.scope-test-6-passed-column1'},
                             {
                                 name: 'sub-column',
                                 scope: 'div:last-child',
                                 collection: [
-                                    {name: 'column2', scope: 'div.get-scope-test-6-passed-column2'},
-                                    {name: 'column3', scope: 'div.get-scope-test-6-passed-column3'},
-                                    {name: 'column4', scope: 'div.get-scope-test-6-passed-column4'}
+                                    {name: 'column2', scope: 'div.scope-test-6-passed-column2'},
+                                    {name: 'column3', scope: 'div.scope-test-6-passed-column3'},
+                                    {name: 'column4', scope: 'div.scope-test-6-passed-column4'}
                                 ]
                             }
                         ]]
@@ -64,17 +68,17 @@ describe('Parser', function () {
             return parser.parse(
                 {
                     rules: {
-                        scope: '.scrollable > .content > div.get-scope-test-6-passed',
+                        scope: '.scrollable > .content > div.scope-test-6-passed',
                         collection: [[
-                            {name: 'column1', scope: 'div.get-scope-test-6-passed-column1'},
+                            {name: 'column1', scope: 'div.scope-test-6-passed-column1'},
                             {
                                 name: 'sub-column',
                                 scope: 'div:last-child',
                                 extract: true,
                                 collection: [
-                                    {name: 'column2', scope: 'div.get-scope-test-6-passed-column2'},
-                                    {name: 'column3', scope: 'div.get-scope-test-6-passed-column3'},
-                                    {name: 'column4', scope: 'div.get-scope-test-6-passed-column4'}
+                                    {name: 'column2', scope: 'div.scope-test-6-passed-column2'},
+                                    {name: 'column3', scope: 'div.scope-test-6-passed-column3'},
+                                    {name: 'column4', scope: 'div.scope-test-6-passed-column4'}
                                 ]
                             }
                         ]]
@@ -82,15 +86,48 @@ describe('Parser', function () {
                 }
             ).then(function (found) {
                     expect(found).to.be.instanceOf(Array);
-                    expect(found).to.have.length.above(1);
+                    expect(found.length).equal(11);
 
-                    _.forEach(found, function (item) {
-                        expect(item.column1).equal('column1');
+                    found.forEach(function (item, i) {
+                        expect(item.column1, 'row' + i).equal('column1');
                         for (var i = 2; i <= 4; i++) {
                             var val = 'column' + i;
-                            expect(item['sub-column'][val]).equal(val);
+                            expect(item['sub-column'][val], 'row' + i).equal(val);
                         }
                     }, this);
+                });
+        });
+    });
+});
+
+describe('Actions', function () {
+    this.timeout(5000);
+    describe('#performForRule', function () {
+        it('perform actions from parsing rules', function () {
+            return env
+                .prepare()
+                .then(function () {
+                    var actions = new Actions({
+                        environment: env
+                    });
+                    return actions.performForRule({
+                            actions: [
+                                {
+                                    type: 'wait',
+                                    scope: 'div.scope-test'
+                                },
+                                {
+                                    type: 'click',
+                                    scope: 'div.scope-test'
+                                },
+                                {
+                                    type: 'wait',
+                                    scope: 'div.scope-test.clicked'
+                                }
+                            ]
+                        },
+                        'body'
+                    );
                 });
         });
     });
