576a725148bae387293b7249835c712934c44b4a
Andrew Balakirev <balakirev.andrey@gmail.com>
2016-06-06 22:13:48 +0300

    Actions Refactoring (#70)
    * - actions are separated into the heap of files
    - you can now build browser bundle just with npm run build, the problem with moment is fixed
    
    * changes after review #70

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
+const webpack = require('webpack');
+
 module.exports = {
-    entry: "./browser.js",
+    entry: './browser.js',
     output: {
         path: __dirname,
-        filename: "browser.bundle.js"
+        filename: "build/browser.bundle.js"
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel'//, // 'babel-loader' is also a legal name to reference
-                //query: {
-                    //presets: ['es2015']
-                //}
+                loader: 'babel'
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|ru/)
+    ]
 };
