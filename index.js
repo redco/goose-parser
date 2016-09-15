1f504c93ae4bc7afdf4fae8459162eb3f6eba857
Andrew Red <maZahaca@users.noreply.github.com>
2016-09-15 21:57:49 +0300

    Added jsdom env (#79)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     Environment: require('./lib/Environment'),
     PhantomEnvironment: require('./lib/PhantomEnvironment'),
+    JsDOMEnvironment: require('./lib/JsDOMEnvironment'),
 
     Parser: require('./lib/Parser')
 };
