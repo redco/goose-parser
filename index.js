5780a474dcbc7d686f3ecc5def33db1b62802f5a
Andrew Red <andrew@reddikh.com>
2017-10-01 00:37:18 +0300

    Added: - Action Not - Transform base64Decode - fixed action for cases (double run when action type of cases)
    :goose:

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = {
     Environment: require('./lib/Environment'),
     PhantomEnvironment: require('./lib/PhantomEnvironment'),
     JsDOMEnvironment: require('./lib/JsDOMEnvironment'),
+    ChromeEnvironment: require('./lib/PuppeteerEnvironment'),
+    BrowserEnvironment: require('./lib/BrowserEnvironment'),
 
     Parser: require('./lib/Parser')
 };
