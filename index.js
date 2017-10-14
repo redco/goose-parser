abef210fab22abfae6c20676bd6f645e0cd64925
Andrew Red <andrew@reddikh.com>
2017-10-14 13:14:50 +0300

    Moving to native promises
    - rewrote parser code without vow
    - used async/await
    - removed envs
    - reduced vendors
    - lodash full lib replaced with single partly libs of lodash
    
    :goose:

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-module.exports = {
-    Environment: require('./lib/Environment'),
-    JsDOMEnvironment: require('./lib/JsDOMEnvironment'),
-    ChromeEnvironment: require('./lib/PuppeteerEnvironment'),
-    BrowserEnvironment: require('./lib/BrowserEnvironment'),
-
-    Parser: require('./lib/Parser')
-};
+module.exports = require('./lib/Parser');
