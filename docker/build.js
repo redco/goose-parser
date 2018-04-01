d7350857a472966c73a2fc17c8ce231c64f8de09
Andrew Red <andrew@reddikh.com>
2018-04-01 12:01:33 +0300

    Added publisher script and fixed build to use specific version of env
    :goose:

--- docker/build.js
+++ docker/build.js
@@ -1,14 +1,37 @@
-const { version } = require('./package.json');
 const fs = require('fs');
+const { exec } = require('child_process');
+const { version } = require('./package.json');
 
-const environment = process.env.ENV || 'goose-phantom-environment';
-const pkg = {
-    private: true,
-    name: 'goose-parser-dockerized',
-    dependencies: {
-      'goose-parser': version,
-      [environment]: 'latest',
-    },
+const getVersion = async (environmentName) => {
+  return new Promise((resolve, reject) => {
+    exec(`npm show ${environmentName} version`, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(stdout);
+    });
+  });
+};
+
+
+(async function () {
+  try {
+    const environmentName = process.env.ENVIRONMENT || 'goose-phantom-environment';
+    const environmentVersion = await getVersion(environmentName);
+    const pkg = {
+      private: true,
+      name: 'goose-parser-dockerized',
+      dependencies: {
+        'goose-parser': version,
+        [environmentName]: `^${environmentVersion}`,
+      },
+    };
+    fs.writeFileSync('./package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+  } catch (e) {
+    console.log('Error occurred');
+    console.log(e.message, e.stack);
+    process.exit(1);
   }
-;
-fs.writeFileSync('./package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+})();
--- docker/publish.sh
+++ docker/publish.sh
@@ -6,23 +6,18 @@ if [ -z "$1" ]
     exit
 fi
 
-allowedEnvironments=("phantom" "chrome" "jsdom")
-environment=`echo "$1" | sed -n 's/goose-\(.*\)-environment/\1/p'`
+environmentName=`echo "$1" | sed -n 's/goose-\(.*\)-environment/\1/p'`
+environmentVersion=`npm show goose-${environmentName}-environment version`;
 
-echo "$environment";
-
-if [[ !" ${allowedEnvironments[*]} " == ` echo "$environment" ` ]]
-then
-  echo "First arg should be valid environment name, one of (goose-phantom-environment, goose-chrome-environment, goose-jsdom-environment)"
-  exit
-fi
-
-#echo "Building image for $1";
-#TAG_NAME="goose-parser";
-#IMAGE_NAME="$TAG_NAME";
-#docker build --build-arg ENVIRONMENT=$1 -t "$IMAGE_NAME" -f ./Dockerfile .
-#DOCKER_NAME="registry.gitlab.com/findexchange/front-page:$TAG_NAME"
-#docker tag "$IMAGE_NAME" "$DOCKER_NAME"
-#docker push "$DOCKER_NAME"
-#docker run --rm -e "TAG_NAME=$TAG_NAME" appropriate/curl -X POST -H 'Content-type:application/json' --data "{\"text\":\"A new version of *front-page* project is available for install\" ,\"link_names\":1,\"username\":\"release-bot\",\"icon_emoji\":\":monkey_face:\",\"attachments\":[{\"text\":\"$TAG_NAME\",\"callback_id\":\"release\",\"color\":\"#B3FF0E\",\"attachment_type\":\"default\"}]}" https://hooks.slack.com/services/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
-#docker rmi "$IMAGE_NAME"
+echo "Building image for $1";
+IMAGE_NAME="redcode/goose-parser";
+TAG_NAME_VERSIONED="${environmentName}-${environmentVersion}";
+TAG_NAME_LATEST="${environmentName}-latest";
+docker build --build-arg ENVIRONMENT=$1 -t "$TAG_NAME_VERSIONED" -t "$TAG_NAME_LATEST" -f ./Dockerfile .
+DOCKER_NAME_VERSIONED="$IMAGE_NAME:$TAG_NAME_VERSIONED"
+DOCKER_NAME_LATEST="$IMAGE_NAME:$TAG_NAME_LATEST"
+docker tag "$TAG_NAME_VERSIONED" "$DOCKER_NAME_VERSIONED"
+docker tag "$TAG_NAME_LATEST" "$DOCKER_NAME_LATEST"
+docker push "$DOCKER_NAME_VERSIONED"
+docker push "$DOCKER_NAME_LATEST"
+docker rmi "$DOCKER_NAME_VERSIONED" "$DOCKER_NAME_LATEST"
