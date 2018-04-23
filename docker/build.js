331231118e43e919556a7e98ca7a8935ed3c8def
Andrew Red <andrew@reddikh.com>
2018-04-23 12:12:20 +0300

    Simplified docker build by using as base one of environments
    :goose:

--- docker/build.js
+++ docker/build.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const { exec } = require('child_process');
-const { version } = require('./package.json');
+const { dependencies: environmentDependencies } = require('./package.json');
 
 const getVersion = async (environmentName) => {
   return new Promise((resolve, reject) => {
-    exec(`npm show ${environmentName} version`, (err, stdout, stderr) => {
+    exec(`npm show ${environmentName} version`, (err, stdout) => {
       if (err) {
         reject(err);
         return;
@@ -17,19 +17,16 @@ const getVersion = async (environmentName) => {
 
 (async function () {
   try {
-    const environmentName = process.env.ENVIRONMENT || 'goose-phantom-environment';
-    const environmentVersion = await getVersion(environmentName);
     const pkg = {
       private: true,
       name: 'goose-parser',
       dependencies: {
-        'goose-parser': version,
-        [environmentName]: `^${environmentVersion}`,
+        ...environmentDependencies,
+        'goose-parser': await getVersion('goose-parser'),
         'minimist': '^1.2.0',
       },
     };
     fs.writeFileSync('./package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
-    fs.writeFileSync('./environment.js', `module.exports = require('${environmentName}');`, 'utf-8');
   } catch (e) {
     console.log('Error occurred');
     console.log(e.message, e.stack);
