b4a00aca418d9409eb8dcfa9ce0197e370445fa8
Andrew Red <andrew@reddikh.com>
2018-05-26 12:01:00 +0300

    updated publishing script
    :goose:

--- docker/publish.sh
+++ docker/publish.sh
@@ -12,13 +12,11 @@ gooseVersion=`npm show goose-parser version`;
 
 echo "Building image for goose-parser based on $1";
 IMAGE_NAME="redcode/goose-parser";
-TAG_NAME_VERSIONED="${environmentName}-${environmentVersion}-parser-${gooseVersion}";
-TAG_NAME_LATEST="${environmentName}-latest-parser-${gooseVersion}";
+TAG_NAME_VERSIONED="${IMAGE_NAME}:${environmentName}-${environmentVersion}-parser-${gooseVersion}";
+TAG_NAME_LATEST="${IMAGE_NAME}:${environmentName}-latest-parser-${gooseVersion}";
 docker build --build-arg ENVIRONMENT=$1 --build-arg ENVIRONMENT_VERSION=$environmentVersion -t "$TAG_NAME_VERSIONED" -t "$TAG_NAME_LATEST" -f ./Dockerfile .
-DOCKER_NAME_VERSIONED="$IMAGE_NAME:$TAG_NAME_VERSIONED"
-DOCKER_NAME_LATEST="$IMAGE_NAME:$TAG_NAME_LATEST"
-docker tag "$TAG_NAME_VERSIONED" "$DOCKER_NAME_VERSIONED"
-docker tag "$TAG_NAME_LATEST" "$DOCKER_NAME_LATEST"
-docker push "$DOCKER_NAME_VERSIONED"
-docker push "$DOCKER_NAME_LATEST"
+docker tag "$TAG_NAME_VERSIONED" "$TAG_NAME_VERSIONED"
+docker tag "$TAG_NAME_LATEST" "$TAG_NAME_LATEST"
+docker push "$TAG_NAME_VERSIONED"
+docker push "$TAG_NAME_LATEST"
 docker rmi "$DOCKER_NAME_VERSIONED" "$DOCKER_NAME_LATEST"
