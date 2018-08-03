a144a12e4e60dd24525b8b0b1ac7c0a419704530
Andrew Red <andrew@reddikh.com>
2018-08-03 12:52:00 +0300

    - fixed coverage config
    :goose:

--- jest.config.js
+++ jest.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    '*/lib/**',
+    'lib/**',
   ],
 
   // The directory where Jest should output its coverage files
